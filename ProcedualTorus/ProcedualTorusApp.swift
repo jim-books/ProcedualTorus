48a1168a2b90d66c455337e7ae43dcf7bca701e0
jimbookshkust <khhungac@connect.ust.hk>
2025-09-04 16:40:46 +0800

    Initial Commit

---
+++ ProcedualTorus/ProcedualTorusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcedualTorusApp.swift
+//  ProcedualTorus
+//
+//  Created by jimbook on 4/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProcedualTorusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProcedualTorusTests/ProcedualTorusTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProcedualTorusTests.swift
+//  ProcedualTorusTests
+//
+//  Created by jimbook on 4/9/2025.
+//
+
+import Testing
+@testable import ProcedualTorus
+
+struct ProcedualTorusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
