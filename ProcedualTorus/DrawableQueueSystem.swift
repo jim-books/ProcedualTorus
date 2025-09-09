de4bbcbb7a2b17490261b64801aa3344534d6f56
jimbookshkust <khhungac@connect.ust.hk>
2025-09-09 12:05:23 +0800

    Drawable Queue system that updates over time for entities with the component.

--- ProcedualTorus/DrawableQueueSystem.swift
+++ ProcedualTorus/DrawableQueueSystem.swift
@@ -5,4 +5,20 @@
 //  Created by jimbook on 9/9/2025.
 //
 
-import Foundation
+import RealityKit
+
+struct DrawableQueueSystem: System {
+    static let query = EntityQuery(where: .has(DrawableQueueComponent.self))
+    
+    init(scene: RealityKit.Scene) { }
+    
+    func update(context: SceneUpdateContext) {
+        for entity in context.entities(matching: Self.query, updatingSystemWhen: .rendering) {
+            if var comp = entity.components[DrawableQueueComponent.self] {
+                comp.update(deltaTime: context.deltaTime)
+                
+                entity.components[DrawableQueueComponent.self] = comp
+            }
+        }
+    }
+}
